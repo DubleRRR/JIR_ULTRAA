de915bef557fc31aec3c0b06cb8e6bdd6e05b977
DubleRRR <nikitacernyh11@gmail.com>
2026-09-15 16:02:33 +0700

    Add files via upload
    Добавлено адекватное окно для вывода результатов

--- Калькуль2.py
+++ Калькуль2.py
@@ -4,7 +4,7 @@ import customtkinter as ctk #Подключил библиотеки
 #Окно
 root = ctk.CTk()
 root.title('Калькулятор эхолокации')
-root.geometry('720x480')
+root.geometry('850x480')
 root.resizable(width=False, height=False)
 
 # Arial Black сделает надписи очень плотными, толстыми и современными
@@ -14,35 +14,56 @@ root.resizable(width=False, height=False)
 #FONT_SMALL = ctk.CTkFont(family="Trebuchet MS", size=12, weight="bold")
 #FONT_RESULT = ctk.CTkFont(family="Trebuchet MS", size=15, weight="bold")
 
+#Большой блок слева
 block = ctk.CTkFrame(root, width=380, height=450, border_width=1, border_color='#CBD5E1', fg_color='#F0F4F8')
 block.grid_propagate(False)
 block.place(x=15, y=15)
 
+#"Разделительная полоса"
 block1 = ctk.CTkFrame(block, width=350, height=120, border_width=1, border_color='#CBD5E1', fg_color='#E2E8F0')
 block1.grid_propagate(False)
 block1.place(x=15, y=265)
 
+#Блок с параметрами воды
 block2 = ctk.CTkFrame(block, width=350, height=4, fg_color='#E2E8F0')
 block2.grid_propagate(False)
 block2.place(x=15, y=242)
 
-label1 = ctk.CTkLabel(block, text='Среда', text_color='#475569', font=("Trebuchet MS", 14))
-label1.grid(row=0, column=0, pady=5, padx=15, sticky='w')
+#Блок результатов справа
+block3 = ctk.CTkFrame(root, width=425, height=178, border_width=1, border_color='#CBD5E1', fg_color='#E2E8F0')
+block3.grid_propagate(False)
+block3.place(x=410, y=15)
 
-label2 = ctk.CTkLabel(block, text='Температура воздуха', text_color='#475569', font=("Comic Sans MC", 14))
-label2.grid(row=3, column=0, pady=5, padx=45, sticky='w')
+#Окно скорости звука
+block4 = ctk.CTkFrame(block3, width=190, height=110, fg_color='#F0F4F8') #border_width=1, border_color='#CBD5E1'
+block4.grid_propagate(False)
+block4.place(x=15, y=50)
 
-label3 = ctk.CTkLabel(block, text='Время распространения сигнала', text_color='#475569', font=("Comic Sans MC", 14))
-label3.grid(row=5, column=0, pady=5, padx=45, sticky='w')
+#Окно расстояния
+block5 = ctk.CTkFrame(block3, width=190, height=110, fg_color='#F0F4F8')
+block5.grid_propagate(False)
+block5.place(x=220, y=50)
 
-label4 = ctk.CTkLabel(block1, text='Параметры воды', text_color='#475569', font=("Comic Sans MC", 14))
+label1 = ctk.CTkLabel(block, text='Среда', text_color='#475569', font=("Century Gothic", 15, 'bold'))
+label1.grid(row=0, column=0, pady=(8,0), padx=15, sticky='w')
+
+label2 = ctk.CTkLabel(block, text='Температура воздуха', text_color='#475569', font=("Century Gothic", 15, 'bold'))
+label2.grid(row=3, column=0, pady=(10,0), padx=45, sticky='w')
+
+label3 = ctk.CTkLabel(block, text='Время распространения сигнала', text_color='#475569', font=("Century Gothic", 15, 'bold'))
+label3.grid(row=5, column=0, pady=(10,0), padx=45, sticky='w')
+
+label4 = ctk.CTkLabel(block1, text='Параметры воды', text_color='#475569', font=("Century Gothic", 15, 'bold'))
 label4.grid(row=0, column=0, pady=(10,0), padx=17, sticky='w')
 
-label5 = ctk.CTkLabel(block1, text='Солёность', text_color='#475569', font=("Comic Sans MC", 12))
+label5 = ctk.CTkLabel(block1, text='Солёность', text_color='#475569', font=("Century Gothic", 15, 'bold'))
 label5.grid(row=1, column=0, pady=(6,0), padx=17, sticky='w')
 
-label6 = ctk.CTkLabel(block1, text='Давление', text_color='#475569', font=("Comic Sans MC", 12))
+label6 = ctk.CTkLabel(block1, text='Глубина', text_color='#475569', font=("Century Gothic", 15, 'bold'))
 label6.grid(row=1, pady=(6,0), padx=183, sticky='w')
+
+label7 = ctk.CTkLabel(block3, text='Результат', text_color='#475569', font=("Century Gothic", 18, 'bold'))
+label7.grid(row=0, padx=15, pady=(11,0))
 
 #ctk.set_appearance_mode("Light")
 #root.configure(fg_color="#F0F4F8")
@@ -75,7 +96,7 @@ def environment_changed(choise): #Чойс - то же говно, что и э�
 environments = ['Воздух', 'Вода'] #Энвироментс - параша, название переменной, именую как хочу. Список - это квадратные скобки
 environment = ctk.CTkComboBox(block, #Комбобокс - лист-выпадашка
                               values=environments, #Валуес - слова в выпадашке
-                              command=environment_changed, height=36, width=350, border_width=1, border_color='#CBD5E1', button_color='#CBD5E1', button_hover_color="#0082FF", text_color='#475569') #wight=430, height=38) #corner_radius = 8  # Мягкое скругление углов
+                              command=environment_changed, height=36, width=350, border_width=1, border_color='#CBD5E1', button_color='#CBD5E1', button_hover_color="#0082FF", text_color='#475569', font=('Century Gothic', 13, 'bold')) #wight=430, height=38) #corner_radius = 8  # Мягкое скругление углов
 environment.set('Воздух') #Сет выбирает слово в кавычках как дефолтное
 environment_changed(environment.get())
 environment.grid(row=1, column=0, padx=15)
@@ -85,18 +106,56 @@ environment.grid(row=1, column=0, padx=15)
 
 #Поля ввода
 e1 = ctk.CTkEntry(block, placeholder_text= '', width=320, height=36, border_width=1, border_color='#CBD5E1') #Время
-e1.grid(row=4, column=0, sticky='e', padx=15)
+e1.grid(row=6, column=0, sticky='e', padx=15)
 
 e2 = ctk.CTkEntry(block, placeholder_text='', width=320, height=36, border_width=1, border_color='#CBD5E1') #Температура
-e2.grid(row=6, column=0, sticky='e', padx=15)
+e2.grid(row=4, column=0, sticky='e', padx=15)
 
 #На лейбл выводится результат
-label = ctk.CTkLabel(root,
+label = ctk.CTkLabel(block4,
               text="",
-              font=("ComicSansMC", 12),
+              font=("Century Gothic", 40, 'bold'),
               text_color='black',
               )
-label.place(x=420, y=100)
+label.place(x=15, y=50)
+
+ms = ctk.CTkLabel(block4,
+              text="м/с",
+              font=("Century Gothic", 20),
+              text_color='#475569',
+              )
+ms.place(x=135, y=70)
+
+skorost = ctk.CTkLabel(block4,
+                       text='Скорость звука',
+                       font=("Century Gothic", 13, 'bold'),
+                       text_color='#475569'
+                       )
+skorost.place(x=30, y=10)
+
+label0 = ctk.CTkLabel(block5,
+              text="",
+              font=("Century Gothic", 40,'bold'),
+              text_color='black',
+              )
+label0.place(x=15, y=50)
+
+m = ctk.CTkLabel(block5,
+              text="м",
+              font=("Century Gothic", 20),
+              text_color='#475569',
+              )
+m.place(x=150, y=70)
+
+rasstoyanie = ctk.CTkLabel(block5,
+                       text='Расстояние',
+                       font=("Century Gothic", 13, 'bold'),
+                       text_color='#475569'
+                       )
+rasstoyanie.place(x=30, y=10)
+
+er=ctk.CTkLabel(block3, text='', font=("Century Gothic", 18), text_color='red')
+er.place(x=130, y=11)
 
 #environment.bind('<<ComboboxSelected>>',
 #environment_changed)
@@ -106,7 +165,7 @@ label.place(x=420, y=100)
     #e2.delete(0, END)
     #solenost.delete(0, END)
     #davlenie.delete(0, END)
-    #label.configure(text="")
+
 
 #btn1 = ctk.CTkButton(root,
              #text = 'Удалить',
@@ -125,7 +184,7 @@ def distanse():
         num1 = float(e1.get()) #Время. Переменная = принять за число(знаки из поля ввода. Вытащить())
         num2 = float(e2.get()) #Температура
         if environment.get()=='Воздух': #Смотрит на значение параши-переменной и смотрит, воздух ли там
-            v=331 + 0.6 * num2
+            v=331.3 + 0.606 * num2
         elif environment.get()=='Вода':
             if solenost.get() in ('') or davlenie.get() in (''): #Если оставил поле пустым, то скорость берётся базовая
                 v = 1480
@@ -134,16 +193,18 @@ def distanse():
                 d = float(davlenie.get())  # Давление/Глубина в метрах
                 v=1448.96 + (4.591 * num2) - (0.05304 * (num2**2)) + (0.0002374 * (num2**3)) + (1.34 * (s - 35)) + (0.0163 * d) #Формула Маккензи
         result = num1 * v / 2
-        label.configure(text=f"Скорость звука: {v:.1f} м/с\nРезультат: {result:.2f} м.", #:.2f округляет до 2 знаков после запятой, n - энтер
-                     text_color='green')
+        label.configure(text=f"{v:.1f}") #\nРезультат: {result:.1f} м.", :.2f округляет до 2 знаков после запятой, n - энтер
+        label0.configure(text=f"{result:.1f}")
+        er.configure(text='')
     except ValueError: #Вместо ошибки прога ругает тебя
-        label.configure(text='Числа пиши осёл.', #Конфигур меняет. В данном случае меняет текст с пустого на инструкцию и имя получателя
-                     text_color='red')
+        er.configure(text='Числа пиши осёл.') #Конфигур меняет. В данном случае меняет текст с пустого на инструкцию и имя получателя
+        label.configure(text="")
+        label0.configure(text="")
 
 btn2 = ctk.CTkButton(block,
              text = 'Рассчитать',
              command = distanse,
-             font = ("ComicSansMS", 15),
+             font = ("Century Gothic", 15, 'bold'),
              #width=10, height=2, - Размеры кнопки
              hover_color = 'blue', #activebackground = hover_color
              fg_color = '#0082FF', #bg, background = fg_color 3a92ff
